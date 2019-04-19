bb4acc43508cec44ddd2da5b554a54eed94b5ec5
Pawan B <31662900+zephyrpawan@users.noreply.github.com>
2019-04-19 13:12:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+    
+    next();
 });
 
 app.use ('/api/posts',(req, res, next)=>{
