a5abffffba5b7b458ab883d7611383579259be6e
Pawan B <31662900+zephyrpawan@users.noreply.github.com>
2019-04-19 13:11:37 -0400

    Update appJS with CORS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 //     res.send('Hello from express!');
 // });
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT, OPTIONS');
+});
+
 app.use ('/api/posts',(req, res, next)=>{
    // res.send("This my posts");
 const posts = [
