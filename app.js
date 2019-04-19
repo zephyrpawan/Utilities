4901519b3f301b168a25010121cd926ed9662471
Pawan B <31662900+zephyrpawan@users.noreply.github.com>
2019-04-19 12:08:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ const app = express();
 //     res.send('Hello from express!');
 // });
 
-app.use ('/posts',(req, res, next)=>{
-    res.send("This my posts");
+app.use ('/api/posts',(req, res, next)=>{
+   // res.send("This my posts");
+const posts = [
+    {
+        id: "uyhafdouqnb",
+        title: "first post",
+        content: "this my first post"
+    },
+    {
+        id: "g7dbjkcboiwefnd",
+        title: "second post",
+        content: "this my second post"
+    }
+];
+   res.status(200).json({
+       message: 'posts fetched successfully',
+       posts: posts
+   });
 });
 
 module.exports = app;
